5c6cfd9ec23df18aacf93cac98d592271597f20a
Anzhalika Turlak <293852647@mail.ru>
2024-02-21 17:23:48 +0300

    feat: add fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -220,8 +220,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const newObj = Object.create(proto);
+  const result = Object.assign(newObj, JSON.parse(json));
+  return result;
 }
 
 /**
